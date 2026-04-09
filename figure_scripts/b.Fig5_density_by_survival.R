ad052363469402c8ea1fde7a0ae358ead9eb9549
Aimee.Fullerton <1365855290113302@mil>
2026-04-09 10:29:37 -0700

    updates to include correlation test (and point to correct data file and column names)

--- figure_scripts/b.Fig5_density_by_survival.R
+++ figure_scripts/b.Fig5_density_by_survival.R
@@ -1,13 +1,19 @@
 # Figure 5: Density versus Survival ----
+options(scipen = 99)
 
 
-density <- read.csv("data/density.csv")
+density <- read.csv("data/density_vs_survival.csv")
 
+cor.test(dat$Parr_density, dat$Parr_to_smolt_survival)
+p <- round(cortest$p.value, 5); if(p < 0.001) p <- "< 0.001"
+r <- round(cortest$estimate, 2)
+plot(dat$Parr_density, dat$Parr_to_smolt_survival, las = 1, ylab = "parr-to-smolt survival", xlab = "parr density in natal habitats", pch = 19)
+legend("topright", legend = paste0("r = ", r, " (p = ", p, ")"), bty = 'n')
 
-png("plots4ms/density_vs_survival.png", width = 6.58, height = 5, units = "in", res = 600)
-x <- density[,"TaggingYear"]
-y1 <- density[, "ParrDensity"]
-y2 <- density[, "Parr2SmoltSurvival"]
+png("plots/density_vs_survival.png", width = 6.58, height = 5, units = "in", res = 600)
+x <- density[,"Year"]
+y1 <- density[, "Parr_density"]
+y2 <- density[, "Parr_to_smolt_survival"]
 par(mar = c(5,5.5,3,5))
 plot(x, y1, las = 1, ylab = "", type = 'b', xlab = "Tagging year", pch = 19, col = "dodgerblue",
      ylim = as.numeric(c(min(y1, na.rm = T), max(y2, na.rm = T))), axes = F)
@@ -20,6 +26,8 @@ plot(x, y2, type = 'b', pch = 17, col = "darkorange", axes = F, xlab = "", ylab 
 axis(side = 4, at = pretty(range(y2, na.rm = T)), las = 1)
 mtext("Parr-to-smolt Survival", side = 4, line = 3, col = "darkorange")
 box()
+legend("topright", legend = paste0("r = ", r, " (p = ", p, ")"), bty = 'n')
 dev.off()
-write.csv(survival_sites, file = "plots4ms/density_vs_survival.csv")
+
+
 
